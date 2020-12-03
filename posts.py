a0e7e710071ee72b7a0449ecab6db6ca4dad8d2c
nilldiggonto <nilldiggonto@gmail.com>
2020-12-04 00:37:56 +0600

    json test

--- posts.py
+++ posts.py
@@ -3,3 +3,10 @@ class Post:
     def __init__(self,title,content):
         self.title = title
         self.content = content
+
+    #json method
+    def json(self):
+        return{
+            'title':self.title,
+            'content':self.content
+        }
--- tests/unit/post_test.py
+++ tests/unit/post_test.py
@@ -7,3 +7,9 @@ class PostTest(TestCase):
 
         self.assertEqual('Test',p.title)
         self.assertEqual('Tests Content',p.content)
+
+    def test_json(self):
+        p = Post('Test','Tests Content')
+        expected = {'title':'Test', 'content': 'Tests Content'}
+
+        self.assertDictEqual(expected, p.json())
